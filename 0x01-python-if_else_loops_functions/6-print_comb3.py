25f6f552c181389da1fc5dc2710bc9c18c73f61c
holat <holatolorunshola@gmail.com>
2022-11-29 21:54:19 +0100

    final

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 10):
     for j in range(0, 10):
         if i < j:
             if i < 8 and j < 10:
-                print(f"{i}{j}", end=', ')
+                print("{}{}".format(i, j), end=', ')
             else:
-                print(f"{i}{j}")
+                print("{}{}".format(i, j))
