759d7771903f7c233c4fe58d34b5873a2cd8a24c
holat <holatolorunshola@gmail.com>
2022-11-30 19:39:21 +0100

    final

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    for i in range(1, b + 1):
-        a *= a
-    return (a)
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+    return (a ** b)
