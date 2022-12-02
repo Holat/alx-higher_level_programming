d02257dec8c7ef1b24e82998337f618d6e72fdb2
holat <holatolorunshola@gmail.com>
2022-12-02 18:09:12 +0100

    final

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
-length = len(argv)
-print(f'{length - 1} {"argument." if length == 1 else "arguments:"}')
+if __name__ == '__main__':
+    length = len(argv)
+    print(f'{length - 1} {"argument." if length == 1 else "arguments:"}')
 
-for i in range(1,length):
-    print(f'{i}: {argv[i]}')
+    for i in range(1,length):
+        print(f'{i}: {argv[i]}')
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
-aSum = 0
-for i in range(1, len(argv)):
-    aSum += int(argv[i])
-print(aSum)
+if __name__ == '__main__':
+    aSum = 0
+    for i in range(1, len(argv)):
+        aSum += int(argv[i])
+    print(aSum)
