eff51d8810a40b3d888ba2bcfb0ea60ec523ee3e
holat <holatolorunshola@gmail.com>
2022-12-02 18:23:27 +0100

    final

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 from sys import argv
 if __name__ == '__main__':
     length = len(argv)
-    print(f'{length - 1} {"argument." if length == 1 else "arguments:"}')
+    print('{} {}'.format(length - 1, "argument." if length == 1 else "arguments:"))
 
     for i in range(1,length):
-        print(f'{i}: {argv[i]}')
+        print('{}: {}'.format(i, argv[i]))
