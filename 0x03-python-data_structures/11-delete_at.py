aa531e02b89726bab913c19a782e5a9d5fdb1b7a
holat <holatolorunshola@gmail.com>
2022-12-05 21:17:59 +0100

    initial

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx > len(my_list):
+        return my_list
+
+    new_list = list(range(len(my_list) - 1))
+    j = 0
+    for i in my_list:
+        if i == my_list[idx]:
+                continue
+        new_list[j] = my_list[j]
+        j += 1
+
+    my_list = list(new_list)
+    return (new_list)
