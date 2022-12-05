e3b78bd9c73201b3cd890fd7498beb3bc3d2b2cd
holat <holatolorunshola@gmail.com>
2022-12-05 19:44:09 +0100

    initial

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j < len(matix[i]) - 1:
+                print('{:d}'.format(matrix[i][j], end=" ")
+            else:
+                print('{:d}'.format(matrix[i][j])
